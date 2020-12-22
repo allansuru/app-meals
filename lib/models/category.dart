c3468806dfe3debe4a97e739e7c39750764ea36c
SuRu <allansuru@hotmail.com>
2020-12-22 16:17:27 -0300

    adicionando pagina de categoria

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,5 +5,6 @@ class Category {
   final String title;
   final Color color;
 
-  const Category({this.id, this.title, this.color});
+  const Category(
+      {@required this.id, @required this.title, @required this.color});
 }
