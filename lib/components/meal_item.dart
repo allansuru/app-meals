47ba186ad0716c0c4547af5f90c4221fe640cbd4
SuRu <allansuru@hotmail.com>
2020-12-30 15:51:57 -0300

    passando argumento na volta de uma tela pra tela anterior

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -11,7 +11,8 @@ class MealItem extends StatelessWidget {
 
   void _selectMeal(BuildContext context) {
     Navigator.of(context)
-        .pushNamed(AppRoutes.MEAl_DETAIL, arguments: this.meal);
+        .pushNamed(AppRoutes.MEAl_DETAIL, arguments: this.meal)
+        .then((value) => print('Retornou ${value}'));
   }
 
   @override
